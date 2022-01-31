f5d8d783307d00bbb5e62ded36949d2169d0140c
Julian Centurion <julian.centurion@mercadolibre.com>
2022-01-31 20:42:42 -0300

    Use table view

--- AppTest/SettingsViewController.swift
+++ AppTest/SettingsViewController.swift
@@ -7,28 +7,46 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var stackView: UIStackView!
-    @IBOutlet weak var buttonsContainer: UIView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        navigationController?.navigationBar.prefersLargeTitles = true
         setUpLabels()
-        setUpViews()
     }
     
     func setUpLabels() {
-        titleLabel.text = StringConstants.settigs
+        navigationItem.title = StringConstants.settigs
     }
     
-    func setUpViews() {
-        stackView.rounded(cornerRadius: 10)
-        buttonsContainer.backgroundColor = .clear
-        buttonsContainer.layer.applyShadow()
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        switch(indexPath.row){
+        case 0:
+            cell.textLabel?.text = StringConstants.language
+            break
+        case 1:
+            cell.textLabel?.text = StringConstants.addUser
+            break
+        case 2:
+            cell.textLabel?.text = StringConstants.lastUser
+            break
+        default:
+            break
+        }
+        
+        return cell
     }
 }
-
