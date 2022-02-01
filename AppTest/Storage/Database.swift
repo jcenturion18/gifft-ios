50556fcc5d360fdf641129a41dd5af6d54396fef
Julian Centurion <julian.centurion@mercadolibre.com>
2022-02-01 17:14:01 -0300

    Implement firestone database

--- AppTest/Storage/Database.swift
+++ AppTest/Storage/Database.swift
@@ -7,37 +7,60 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 protocol DataBaseInterface {
     static func setUpProvider()
-    static func store(userName: String)
+    func store(userName: String)
+    func loadUserName(callback: @escaping(User)->Void)
+}
+
+struct User: Codable {
+    let name: String?
 }
 
 class DataBaseImp : DataBaseInterface {
     
+    let db = Firestore.firestore()
+    
     static func setUpProvider() {
         FirebaseApp.configure()
-
-        let db = Firestore.firestore()
-
-        db.collection("users").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
+    }
+    
+    func store(userName: String) {
+        _ = db.collection("users")
+            .addDocument(data: ["name": userName]) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
                 }
             }
+    
+    }
+    
+    func loadUserName(callback: @escaping(User)->Void) {
+        db.collection("users")
+            .whereField("name", isNotEqualTo: "")
+            .order(by: "name", descending: true).limit(to: 1)
+            .getDocuments() { (response, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                }
+                
+                let result = Result {
+                    try response?.documents[0].data(as: User.self)
+                }
+                
+                switch result {
+                    case .success(let user):
+                        if let user = user {
+                            callback(user)
+                        }
+                    break
+                    case .failure(let error):
+                        print("Error decoding user: \(error)")
+                    break
+                }
         }
-       
-    }
-    
-    static func store(userName: String) {
-        
-    }
-    
-    static func loadUserName() -> String {
-        return ""
     }
 }
 
