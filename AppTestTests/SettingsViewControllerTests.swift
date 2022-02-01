cc63f834e643c166c86db11432e482bcd75ee86e
Julian Centurion <julian.centurion@mercadolibre.com>
2022-02-01 18:54:32 -0300

    Create basic tests

--- AppTestTests/SettingsViewControllerTests.swift
+++ AppTestTests/SettingsViewControllerTests.swift
@@ -6,27 +6,34 @@
 //
 
 import XCTest
+@testable import AppTest
 
 class SettingsViewControllerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testBasicControllerConfiguration() throws {
+        
+        let localizableMock = LocalizableMock()
+        localizableMock.saveLanguage(LocalizableOptions.english)
+        let wordings = Wordings(localizable: localizableMock)
+        
+        let database = DatabaseMock()
+        database.store(userName: "Julian")
+        
+        
+        let viewController = SettingsViewController()
+            .with(wordings: wordings)
+            .with(localizable: localizableMock)
+            .with(database: database)
+        
+        _ = viewController.view
+        
+        let lastUserCell = viewController.tableView.cellForRow(at: IndexPath(row: 2, section: 0))
+        XCTAssertEqual("Last user: Julian", lastUserCell!.textLabel!.text)
+        
+        XCTAssertEqual("Settings", viewController.navigationItem.title)
+        
+        viewController.changeLanguage()
+        XCTAssertEqual("Einstellungen", viewController.navigationItem.title)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
+
